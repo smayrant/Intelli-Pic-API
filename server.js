ffaf64d33800e47e6bc9654d47f7afc375f596c2
Sheldrick <mrmayrant@yahoo.com>
2019-07-18 16:28:10 -0400

    set up the skeleton for the login and register routes before connecting to the DB

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+	users: [
+		{
+			id: "123",
+			name: "john",
+			email: "john@gmail.com",
+			password: "pass",
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: "124",
+			name: "sally",
+			email: "sally@gmail.com",
+			password: "pass2",
+			entries: 0,
+			joined: new Date()
+		}
+	]
+};
 
 app.get("/", (req, res) => {
-	res.send("this is working");
+	res.send(database.users);
 });
 
 app.post("/login", (req, res) => {
-	res.json("logging in");
+	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+		res.json("logged in");
+	} else {
+		res.status(400).json("error logging in");
+	}
+});
+
+app.post("/register", (req, res) => {
+	const { name, email, password } = req.body;
+	database.users.push({
+		id: "125",
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3000, () => {
