17329209d4c0645ca7f640a5274093ca935b3f5a
Sheldrick <mrmayrant@yahoo.com>
2019-07-19 22:45:21 -0400

    setup the register and login routes to correctly interact with the DB

--- server.js
+++ server.js
@@ -20,56 +20,63 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: "123",
-			name: "john",
-			email: "john@gmail.com",
-			password: "pass",
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: "124",
-			name: "sally",
-			email: "sally@gmail.com",
-			password: "pass2",
-			entries: 0,
-			joined: new Date()
-		}
-	]
-};
-
 app.get("/", (req, res) => {
 	res.send(database.users);
 });
 
 app.post("/login", (req, res) => {
-	const saltRounds = 10;
-
-	bcrypt.hash(password, saltRounds, function (err, hash) {});
-	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json("Error logging in");
-	}
+	db
+		.select("email", "hash")
+		.from("login")
+		.where("email", "=", req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db
+					.select("*")
+					.from("users")
+					.where("email", "=", req.body.email)
+					.then(user => {
+						res.json(user[0]);
+					})
+					.catch(err => res.status(400).json("Unable to retrieve user"));
+			} else {
+				res.status(400).json("Incorrect credentials");
+			}
+		})
+		.catch(err => res.status(400).json("Incorrect credentials"));
 });
 
 // the user's registration data is received from the body, password is hashed and the new user is stored within the DB
 app.post("/register", (req, res) => {
-	const { name, email, password } = req.body;
-	db("users")
-		.returning("*")
-		.insert({
-			email: email,
-			name: name,
-			joined: new Date()
+	const { email, name, password } = req.body;
+	const saltRounds = 10;
+	const hash = bcrypt.hashSync(password, saltRounds);
+	db
+		.transaction(trx => {
+			trx
+				.insert({
+					hash: hash,
+					email: email
+				})
+				.into("login")
+				.returning("email")
+				.then(loginEmail => {
+					return trx("users")
+						.returning("*")
+						.insert({
+							email: loginEmail[0],
+							name: name,
+							joined: new Date()
+						})
+						.then(user => {
+							res.json(user[0]);
+						});
+				})
+				.then(trx.commit)
+				.catch(trx.rollback);
 		})
-		.then(user => {
-			res.json(user[0]);
-		})
-		.catch(err => res.status(400).json("Unable to register this user"));
+		.catch(err => res.status(400).json("Unable to register"));
 });
 
 // retrieve a user based on their id stored within the DB
@@ -93,17 +100,14 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-	if (!found) {
-		res.status(400).json("not found");
-	}
+	db("users")
+		.where("id", "=", id)
+		.increment("entries", 1)
+		.returning("entries")
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json("Unable to retrieve user entries"));
 });
 
 app.listen(3000, () => {
