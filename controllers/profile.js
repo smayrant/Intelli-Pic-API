b87e7054244089554c66e53a4d77d3bdfd30b459
Sheldrick <mrmayrant@yahoo.com>
2019-07-20 04:31:06 -0400

    cleaned up the server file

---
+++ controllers/profile.js
@@ -0,0 +1,21 @@
+const handleGetProfile = (req, res, db) => {
+	const { id } = req.params;
+	db
+		.select("*")
+		.from("users")
+		.where({
+			id: id
+		})
+		.then(user => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json("User not found");
+			}
+		})
+		.catch(err => res.status(400).json("Error retrieving user"));
+};
+
+module.exports = {
+	handleGetProfile
+};
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const knex = require("knex");
+const register = require("./controllers/register");
+const login = require("./controllers/login");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
 	client: "pg",
@@ -25,89 +29,21 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-	db
-		.select("email", "hash")
-		.from("login")
-		.where("email", "=", req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if (isValid) {
-				return db
-					.select("*")
-					.from("users")
-					.where("email", "=", req.body.email)
-					.then(user => {
-						res.json(user[0]);
-					})
-					.catch(err => res.status(400).json("Unable to retrieve user"));
-			} else {
-				res.status(400).json("Incorrect credentials");
-			}
-		})
-		.catch(err => res.status(400).json("Incorrect credentials"));
+	login.handleLogin(req, res, db, bcrypt);
 });
 
 // the user's registration data is received from the body, password is hashed and the new user is stored within the DB
 app.post("/register", (req, res) => {
-	const { email, name, password } = req.body;
-	const saltRounds = 10;
-	const hash = bcrypt.hashSync(password, saltRounds);
-	db
-		.transaction(trx => {
-			trx
-				.insert({
-					hash: hash,
-					email: email
-				})
-				.into("login")
-				.returning("email")
-				.then(loginEmail => {
-					return trx("users")
-						.returning("*")
-						.insert({
-							email: loginEmail[0],
-							name: name,
-							joined: new Date()
-						})
-						.then(user => {
-							res.json(user[0]);
-						});
-				})
-				.then(trx.commit)
-				.catch(trx.rollback);
-		})
-		.catch(err => res.status(400).json("Unable to register"));
+	register.handleRegister(req, res, db, bcrypt);
 });
 
 // retrieve a user based on their id stored within the DB
 app.get("/profile/:id", (req, res) => {
-	const { id } = req.params;
-	db
-		.select("*")
-		.from("users")
-		.where({
-			id: id
-		})
-		.then(user => {
-			if (user.length) {
-				res.json(user[0]);
-			} else {
-				res.status(400).json("User not found");
-			}
-		})
-		.catch(err => res.status(400).json("Error retrieving user"));
+	profile.handleGetProfile(req, res, db);
 });
 
 app.put("/image", (req, res) => {
-	const { id } = req.body;
-	db("users")
-		.where("id", "=", id)
-		.increment("entries", 1)
-		.returning("entries")
-		.then(entries => {
-			res.json(entries[0]);
-		})
-		.catch(err => res.status(400).json("Unable to retrieve user entries"));
+	image.handleImage(req, res, db);
 });
 
 app.listen(3000, () => {
